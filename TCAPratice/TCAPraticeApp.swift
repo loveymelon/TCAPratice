1d71c87778a22f0d0c8daedc3fa00c05db51b900
loveymelon <manciepmc@gmail.com>
2024-07-22 14:18:17 +0900

    Initial Commit

---
+++ TCAPratice/TCAPraticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPraticeApp.swift
+//  TCAPratice
+//
+//  Created by 김진수 on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TCAPraticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
