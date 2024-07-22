98726654d319eede88f5063f6cde98fbad813f3f
loveymelon <manciepmc@gmail.com>
2024-07-22 20:42:08 +0900

    Create AddContactFeature

--- TCAPratice/Container/AddContactFeature.swift
+++ TCAPratice/Container/AddContactFeature.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct AddContactFeature {
+    
+    @ObservableState
+    struct State: Equatable {
+        var contact: Contact
+    }
+    
+    enum Action {
+        case cancelButtonTapped
+        case saveButtonTapped
+        case setName(String)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case let .setName(name):
+                state.contact.name = name
+            case .cancelButtonTapped:
+                break
+            case .saveButtonTapped:
+                break
+            }
+            
+            return .none
+        }
+    }
+}
